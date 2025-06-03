247129c936d0f78a9f9671644d3112bfd8b6826f
Vis <viswanatha@onequince.com>
2025-06-03 23:00:36 +0530

    add access code

--- wb/main.py
+++ wb/main.py
@@ -1,9 +1,46 @@
 import os
+import secrets
+from typing import Optional
 
 import tornado.ioloop
 import tornado.web
 
-class MainHandler(tornado.web.RequestHandler):
+# Generate a random token at startup
+ACCESS_TOKEN = os.environ.get('WB_ACCESS_TOKEN') or secrets.token_urlsafe(32)
+print(f"Access token: {ACCESS_TOKEN}")
+
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self) -> Optional[str]:
+        return self.get_secure_cookie("user")
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        if self.current_user:
+            self.redirect("/")
+            return
+        self.write('''
+        <html>
+            <head><title>Login</title></head>
+            <body>
+                <h2>Login</h2>
+                <form method="POST">
+                    <input type="password" name="token" placeholder="Enter access token">
+                    <input type="submit" value="Login">
+                </form>
+            </body>
+        </html>
+        ''')
+
+    def post(self):
+        token = self.get_argument("token", "")
+        if token == ACCESS_TOKEN:
+            self.set_secure_cookie("user", "authenticated")
+            self.redirect("/")
+        else:
+            self.write("<html><body>Invalid token. <a href='/login'>Try again</a></body></html>")
+
+class MainHandler(BaseHandler):
+    @tornado.web.authenticated
     def get(self, path):
         abspath = os.path.abspath(os.path.join(os.getcwd(), path))
         root = os.getcwd()
@@ -33,19 +70,27 @@ class MainHandler(tornado.web.RequestHandler):
             self.set_status(404)
             self.write("File not found")
 
-def make_app():
+def make_app(settings):
+   
     return tornado.web.Application([
+        (r"/login", LoginHandler),
         (r"/(.*)", MainHandler),
-    ])
+    ], **settings)
 
 
 def main():
-    app = make_app()
+    settings = {
+        "cookie_secret": ACCESS_TOKEN,
+        "login_url": "/login",
+    }
+    app = make_app(settings)
     port = 8000
     while port<9000:
         try:
+            print(settings,port)
             app.listen(port)
             print(f"Serving HTTP on 0.0.0.0 port {port} (http://0.0.0.0:{port}/) ...")
+
             tornado.ioloop.IOLoop.current().start()
             
         except:
